38bfbacacdf2b0f61d5a7460aebc9c7b167821c8
sam <3161728@gmail.com>
2020-12-22 22:18:32 +0900

    Add border bottom marker on top header bar

--- src/Components/Router.js
+++ src/Components/Router.js
@@ -3,12 +3,12 @@ import { BrowserRouter, Route, Redirect, Switch } from "react-router-dom";
 import Home from "Routes/Home";
 import TV from "Routes/TV";
 import Search from "Routes/Search";
-import Header from "Components/Header";
+import Topbar from "Components/Header";
 
 const Router = () => (
   <BrowserRouter>
     <>
-    <Header />
+    <Topbar />
     <Switch>
       <Route path="/" exact component={Home} />
       <Route path="/tv" component={TV} />
